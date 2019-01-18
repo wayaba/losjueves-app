abbbca05f695a7854e275a27597b9bc83cd7a282
Pablo <ppedraza.sofrecom@supervielle.com>
2019-01-18 17:29:34 -0300

    subo cosas

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 import { LosjuevesApiService } from '../losjueves-api.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-detail',
@@ -10,34 +11,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DetailPage implements OnInit {
 
+  @ViewChild('doughnutCanvas') doughnutCanvas;
   playerDetail: any[] = [];
-  player: any;
+  doughnutChart: any;
+
   constructor(public api: LosjuevesApiService,
     public loadingController: LoadingController,
     public route: ActivatedRoute,
-    public router: Router) { }
+    public router: Router) { 
+    }
 
     ngOnInit() {
       this.getPlayerDetail();
-      this.getPlayer();
     }
   
-    async getPlayer() {
-      const loading = await this.loadingController.create({
-        message: 'Loading...'
-      });
-      await loading.present();
-      await this.api.getPlayer(this.route.snapshot.paramMap.get('id'))
-        .subscribe(res => {
-          console.log(res);
-          this.player = res;
-          loading.dismiss();
-        }, err => {
-          console.log(err);
-          loading.dismiss();
-        });
-    }
-
     async getPlayerDetail() {
       const loading = await this.loadingController.create({
         message: 'Loading...'
@@ -47,6 +34,7 @@ export class DetailPage implements OnInit {
         .subscribe(res => {
           console.log(res);
           this.playerDetail = res;
+          this.getGraph(res);
           loading.dismiss();
         }, err => {
           console.log(err);
@@ -54,11 +42,47 @@ export class DetailPage implements OnInit {
         });
     }
 
-    getStyle = function(name){
-      if(name == "Azul")
-        return {'color':'primary'};
-      if(name == "Rojo")
-        return {'color':'danger'};
-    };
+    getGraph(res: any){
+      
+      let win = 0;
+      let draw = 0;
+      let lose = 0;
+      let pj = 0;
+      for (var i=0 ; i < res.detail.length; i++) {          
+          win = +win + +res.detail[i].win;
+          draw = +draw + +res.detail[i].draw;
+          lose = +lose + +res.detail[i].lose;
+          pj = +pj + +res.detail[i].pj;
+      }
+
+      if(pj > 0){
+        win = Math.round((win * 100)/pj);
+        draw = Math.round((draw * 100)/pj);
+        lose = Math.round((lose * 100)/pj);
+      }
+      
+      this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+ 
+        type: 'doughnut',
+        data: {
+            labels: ["Ganado","Empatado", "Perdido"],
+            datasets: [{
+                label: '# of Votes',
+                data: [win, draw, lose],
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                hoverBackgroundColor: [
+                    "#FFCE56",  
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }]
+        }
+
+      });
+    }
 
 }
